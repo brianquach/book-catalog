6253013571f1c7e859b36d1cc32451feef4bdbbb
Brian Quach <13rianquach@gmail.com>
2016-01-05 22:37:08 -0800

    chore: add client_secret from google for OAuth

--- vagrant/catalog/catalog/__init__.py
+++ vagrant/catalog/catalog/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+# This app's client Id to generate an access token from Google's OAuth API
+CLIENT_ID = json.loads(
+    open('client_secret.json', 'r').read()
+)['web']['client_id']
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catalog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from catalog import views, models
+from catalog import views
