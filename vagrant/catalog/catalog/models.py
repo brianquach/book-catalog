d0555777da108151c27fd905e2e59988ff2f49f5
Brian Quach <13rianquach@gmail.com>
2016-01-16 17:37:22 -0800

    Style & fix: added documentation and removed unused class init args
    Added class, method, and property documentation. Pep8 check. Removed properties
    that did not need to be initialized.

--- vagrant/catalog/catalog/models.py
+++ vagrant/catalog/catalog/models.py
@@ -1,7 +1,21 @@
+"""
+Copyright 2016 Brian Quach
+Licensed under MIT (https://github.com/brianquach/udacity-nano-fullstack-catalog/blob/master/LICENSE)  # noqa
+"""
 from catalog import db
 
 
 class User(db.Model):
+    """User class represents a Catalog App user.
+
+    Attributes:
+        id: An integer of user's id.
+        name: A string of user's name.
+        email: A string of user's email address.
+        picture: A string of URI to user's picture.
+        catagory_items: A one-to-many relationship between User and
+          CatagoryItems; contains a list of catagory_items created by user.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
     email = db.Column(db.String(1000), nullable=False)
@@ -12,17 +26,36 @@ class User(db.Model):
         lazy='dynamic'
     )
 
-    def __init__(self, name, email, picture, items=[]):
+    def __init__(self, name, email, picture):
+        """Initialize User.
+
+        Args:
+          name: user's name.
+          email: user's email address.
+          picture: URI to user's picture.
+        """
         self.name = name
         self.email = email
         self.picture = picture
-        self.catagory_items = items
 
     def __repr__(self):
+        """Represents User.
+
+        Returns:
+          A string that respresents the User object.
+        """
         return '<User {0}>'.format(self.email)
 
     @property
     def serialize(self):
+        """Serialize User object into a dictionary.
+
+        Makes it easier to convert User class into json or xml form.
+
+        Returns:
+          A dictionary that holds key/value pairs that represent the User
+          class.
+        """
         return {
             'id': self.id,
             'name': self.name,
@@ -32,6 +65,14 @@ class User(db.Model):
 
 
 class Catagory(db.Model):
+    """Catagory class represents a Catalog App catagory.
+
+    Attributes:
+        id: An integer of catagory's id.
+        name: A string of catgory's name.
+        catagory_items: A one-to-many relationship between a Catagory and
+          CatagoryItem; contains a list of items grouped by the catagory.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
     catagory_items = db.relationship(
@@ -40,15 +81,32 @@ class Catagory(db.Model):
         lazy='dynamic'
     )
 
-    def __init__(self, name, items=[]):
+    def __init__(self, name):
+        """Initialize Catagory.
+
+        Args:
+          name: catagory's name.
+        """
         self.name = name
-        self.catagory_items = items
 
     def __repr__(self):
+        """Represents Catagory.
+
+        Returns:
+          A string that respresents the Catagory object.
+        """
         return '<Catagory {0}>'.format(self.name)
 
     @property
     def serialize(self):
+        """Serialize Catagory object into a dictionary.
+
+        Makes it easier to convert Catagory class into json or xml form.
+
+        Returns:
+          A dictionary that holds key/value pairs that represent the Catagory
+          class.
+        """
         return {
             'id': self.id,
             'name': self.name,
@@ -57,6 +115,17 @@ class Catagory(db.Model):
 
 
 class CatagoryItem(db.Model):
+    """CatagoryItem class represents a Catalog App item.
+
+    Attributes:
+      id: An integer of item's id.
+      name: A string of item's name.
+      author: A string of item's author (for books).
+      description: A string of item's description.
+      picture: A string of URI to item's picture.
+      user_id: An integer of the user's id who created the item.
+      catagory_id: An integer of the catagory that item belongs to.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
     author = db.Column(db.String(1000))
@@ -67,18 +136,41 @@ class CatagoryItem(db.Model):
 
     def __init__(self, name, author, description, picture, catagory_id,
                  user_id=None):
+        """Initialize CatagoryItem.
+
+        Args:
+          name: name of item.
+          author: author--for books--of item.
+          description: description of item.
+          picture: URI to picture of item.
+          catagory_id: id of the catagory that the item belongs in.
+          user_id: id of the user that created the item.
+        """
         self.name = name
         self.author = author
         self.description = description
         self.picture = picture
-        self.user_id = user_id
         self.catagory_id = catagory_id
+        self.user_id = user_id
 
     def __repr__(self):
+        """Represents CatagoryItem.
+
+        Returns:
+          A string that respresents the CatagoryItem object.
+        """
         return '<CatagoryItem {0}>'.format(self.name)
 
     @property
     def serialize(self):
+        """Serialize CatagoryItem object into a dictionary.
+
+        Makes it easier to convert CatagoryItem class into json or xml form.
+
+        Returns:
+          A dictionary that holds key/value pairs that represent the
+          CatagoryItem class.
+        """
         return {
             'id': self.id,
             'name': self.name,
