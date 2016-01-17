4cc84f982d3a7cc9cca2f55a33827ce453bc9edc
Brian Quach <13rianquach@gmail.com>
2016-01-16 19:31:09 -0800

    fix: add unique constraint to email column of user table
    User email must be unique because oauth login is based on the username being an
    email.

--- vagrant/catalog/catalog/models.py
+++ vagrant/catalog/catalog/models.py
@@ -18,7 +18,7 @@ class User(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
-    email = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(1000), nullable=False, unique=True)
     picture = db.Column(db.String(1000))
     catagory_items = db.relationship(
         'CatagoryItem',
